eb0cefbeaefb4fc7c51d9d50fe4fafda257d1222
deniskamurua <6597denis@gmail.com>
2024-05-26 19:51:21 +0300

    firstcommitt

--- examProject/settings.py
+++ examProject/settings.py
@@ -17,9 +17,9 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 SECRET_KEY = 'd*8x97+1&+ea&x9=*1ay*b6v&lr14u7l*g6*+#b@oummi^&&z^'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['0.0.0.0']
 
 INSTALLED_APPS = [
     'django.contrib.admin',
